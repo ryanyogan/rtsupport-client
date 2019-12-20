017a6ab431584bec0aeb5511a02ec7e4553fc7a2
ryanyogan <ryanyogan@gmail.com>
2019-12-20 08:58:06 -0800

    Switched app to standard class component for now, too lazy to care about react

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import ChannelSection from "./components/channels/ChannelSection";
 import MessageSection from "./components/messages/MessageSection";
 import UserSection from "./components/users/UserSection";
 
-function App() {
-  const [channels, setChannels] = useState([]);
-  const [activeChannel, setActiveChannel] = useState({});
-  const [users, setUsers] = useState([]);
-  const [messages, setMessages] = useState([]);
-
-  const addChannel = name => {
-    const newChannels = [{ id: channels.length, name }, ...channels];
-    setChannels(newChannels);
+class App extends Component {
+  state = {
+    channels: [],
+    users: [],
+    messages: [],
+    activeChannel: {},
+    connected: false
   };
 
-  const onAddUser = user => {
+  onMessageAdd = message => {
+    const { messages } = this.state;
+
+    messages.push(message);
+    this.setState({ messages });
+  };
+
+  onRemoveUser = removeUser => {
+    const { users } = this.state;
+    const filteredUsers = users.filter(user => user.id !== removeUser.id);
+
+    this.setState({ users: filteredUsers });
+  };
+
+  addChannel = name => {
+    this.ws.removeEventListener("channel add", { name });
+  };
+
+  onAddUser = user => {
+    const { users } = this.state;
+
     const newUsers = [{ user }, ...users];
-    setUsers(newUsers);
+    this.setState({ users: newUsers });
   };
 
-  const setChannel = channel => {
-    setActiveChannel(channel);
+  setChannel = activeChannel => {
+    this.setState({ activeChannel });
   };
 
-  const setUserName = name => {
+  setUserName = name => {
+    const { users } = this.state;
     // TODO: Implement a socket to the server
-    const newUsers = [{ id: users.length, name }, ...users];
-    setUsers(newUsers);
+    const newUser = { id: users.length, name };
+    users.push(newUser);
+    this.setState({ users });
   };
 
-  const addMessage = body => {
+  addMessage = body => {
+    const { messages, users } = this.state;
     // TODO: this.socket.emit('message add', {channelId: activeChannel.id, body })
     const createdAt = new Date();
     const author = users.length > 0 ? users[0].name : "anonymous";
@@ -37,28 +58,79 @@ function App() {
       { id: messages.length, body, createdAt, author },
       ...messages
     ];
-    console.log(newMessages);
-    setMessages(newMessages);
+
+    this.setState({ messages: newMessages });
   };
 
-  return (
-    <div className="app">
-      <div className="nav">
-        <ChannelSection
+  newChannel = channel => {
+    const { channels } = this.state;
+    channels.push(channel);
+    this.setState({ channels });
+  };
+
+  addChannel = name => {
+    const { channels } = this.state;
+
+    const msg = {
+      name: "channel add",
+      data: {
+        id: channels.length,
+        name
+      }
+    };
+    this.ws.send(JSON.stringify(msg));
+  };
+
+  handleMessage = message => {
+    const event = JSON.parse(message.data);
+
+    switch (event.name) {
+      case "channel add":
+        this.newChannel(event.data);
+        break;
+
+      default:
+        console.log("F");
+    }
+  };
+
+  open = () => {
+    this.setState({ connected: true });
+  };
+
+  close = () => {
+    this.setState({ connected: false });
+  };
+
+  componentDidMount() {
+    const ws = (this.ws = new WebSocket("ws://echo.websocket.org"));
+    ws.onmessage = this.handleMessage;
+    ws.onopen = this.open;
+    ws.onclose = this.close;
+  }
+
+  render() {
+    const { activeChannel, channels, users, messages } = this.state;
+
+    return (
+      <div className="app">
+        <div className="nav">
+          <ChannelSection
+            activeChannel={activeChannel}
+            setChannel={this.setChannel}
+            channels={channels}
+            addChannel={this.addChannel}
+          />
+          <UserSection users={users} setUserName={this.setUserName} />
+        </div>
+        <MessageSection
+          addMessage={this.addMessage}
+          messages={messages}
           activeChannel={activeChannel}
-          setChannel={setChannel}
-          channels={channels}
-          addChannel={addChannel}
         />
-        <UserSection users={users} setUserName={setUserName} />
       </div>
-      <MessageSection
-        addMessage={addMessage}
-        messages={messages}
-        activeChannel={activeChannel}
-      />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
